99990b070afc5cce9d2dea11cf64612ec7975669
ShannonB320 <84356531+ShannonB320@users.noreply.github.com>
2021-10-11 20:06:53 -0500

    Completed Chapter 12 Studio

---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,39 @@
+package org.launchcode.spaday.models;
+
+public class User {
+
+    private String userName;
+    private String userEmail;
+    private String userPassword;
+
+    public User(String name, String email, String password) {
+        this.userName = name;
+        this.userEmail = email;
+        this.userPassword = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+}
