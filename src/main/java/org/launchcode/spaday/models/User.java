5676a9a0018b54366e971e47b5a8bf7534c74bba
ShannonB320 <84356531+ShannonB320@users.noreply.github.com>
2021-10-14 20:14:39 -0500

    Completed chapter 13 Studio & Bonus Mission

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,23 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+import java.util.Objects;
+
 public class User {
+
+    @NotBlank(message = "Must not be blank!")
+    @Size(min = 5, max = 15, message = "Username must contain 5-15 characters.")
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "Must not be blank!")
+    @Size(min = 6, message = "Password must be at least 6 characters.")
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
@@ -38,5 +52,24 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
     }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if (!Objects.isNull(this.password) && !Objects.isNull(this.verifyPassword)) {
+            if(!this.password.equals(this.verifyPassword)){
+                this.verifyPassword = null;
+            }
+        }
+    }
+
 }
